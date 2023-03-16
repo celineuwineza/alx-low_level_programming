bba645b353bb50a61288ee2c728f683261281c3b
celineuwineza <nezachandu@gmail.com>
2023-03-16 11:31:27 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the main function
+ * Return 0 (success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (0);
+}
