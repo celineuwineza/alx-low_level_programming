904b65b32ecf3970cedf2a237c4b16f5656944fc
celineuwineza <nezachandu@gmail.com>
2023-03-28 08:30:06 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,7 +8,7 @@ int main(void)
 {
 
 	char password[PASSWORD_LENGTH + 1];
-	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const char charset[] = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
 
 	srand(time(NULL));
 
