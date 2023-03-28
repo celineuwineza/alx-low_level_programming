07689049cd4e5f47bcc86448dc563f7c55b1f4ec
celineuwineza <nezachandu@gmail.com>
2023-03-28 08:28:28 -0700

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+int main(void)
+{
+
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % sizeof(charset)];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
